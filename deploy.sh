40d3324fd0a2d930b29cd52330319d449d293879
Robert Klotzner <robert.klotzner@gmx.at>
2015-02-25 10:36:02 +0100

    Fixed bug in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@
 cabal clean
 cabal configure
 cabal build
+scp -r config mybabymonitor.org:
+scp -r static mybabymonitor.org:
 cd dist/build/mybabymonitor.org
 strip mybabymonitor.org
 gzip mybabymonitor.org
 scp mybabymonitor.org.gz mybabymonitor.org:
-scp -r config mybabymonitor.org:
-scp -r static mybabymonitor.org:
 ssh mybabymonitor.org "gunzip -f mybabymonitor.org.gz"
 #ssh mybabymonitor.org "killall mybabymonitor.org"
 #ssh mybabymonitor.org "(APPROOT=\"http://mybabymonitor.org\" nohup ./mybabymonitor.org &); disown -a"
