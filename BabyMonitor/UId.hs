c267b1eb5416b7aeffbee33a7078c06134ec43bf
Robert Klotzner <robert.klotzner@gmx.at>
2015-04-05 01:33:40 +0200

    Use Text instead of String

--- BabyMonitor/UId.hs
+++ BabyMonitor/UId.hs
@@ -5,11 +5,10 @@ module BabyMonitor.UId (makeUid, fromUserString, toUserString, Uid) where
 
 import System.UUID.V1
 import ClassyPrelude
-import Data.List.Split
 import qualified Codec.Binary.Base32 as B32
-import qualified Data.ByteString.Char8 as BC
 import Data.Either.Combinators
 import Data.Binary
+import qualified Data.Text as T
 
 
 -- Shortened version of V1 UUID.  We drop the MAC address, as for now
@@ -23,19 +22,19 @@ import Data.Binary
 -- ids. 10 bytes is still pretty long and we will use a base 32 code to
 -- reduce it to 16 displayed digits, which is manageable.
 
-newtype Uid = Uid ByteString 
+newtype Uid = Uid ByteString
 
 
 -- Convert an uid to a string suitable to be displayed to a user
-toUserString :: Uid -> String
-toUserString (Uid uid) = intercalate "-" . chunksOf 4
+toUserString :: Uid -> Text
+toUserString (Uid uid) = T.intercalate "-" . T.chunksOf 4
                          . toLower
-                         . BC.unpack . B32.encode
+                         . decodeUtf8 . B32.encode
                          . reverseTimeLow $ uid
 
-fromUserString :: String -> Maybe Uid
+fromUserString :: Text -> Maybe Uid
 fromUserString =  fmap (Uid . reverseTimeLow) . rightToMaybe
-                  . B32.decode . BC.pack
+                  . B32.decode . encodeUtf8
                   . toUpper
                   . filter (/= '-')
          
